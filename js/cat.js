a9db0763f6aba3a2b7df6ef568ab752db72b3cd4
nicolas-kings <1443733886@qq.com>
2022-09-25 14:23:18 +0800

    Site updated: 2022-09-25 14:23:12

--- js/cat.js
+++ js/cat.js
@@ -1,94 +1,146 @@
 if (document.body.clientWidth > 992) {
-    function a() {
-        var a = $(window).height(),
-            b = $("body")[0].scrollHeight,
-            c = $(window).scrollTop(),
-            d = b - a,
-            e = c / (b - a) * 100;
+    function getBasicInfo() {
+        /* 窗口高度 */
+        var ViewH = $(window).height();
+        /* document高度 */
+        var DocH = $("body")[0].scrollHeight;
+        /* 滚动的高度 */
+        var ScrollTop = $(window).scrollTop();
+        /* 可滚动的高度 */
+        var S_V = DocH - ViewH;
+        var Band_H = ScrollTop / (DocH - ViewH) * 100;
         return {
-            ViewH: a,
-            DocH: b,
-            ScrollTop: c,
-            Band_H: e,
-            S_V: d
+            ViewH: ViewH,
+            DocH: DocH,
+            ScrollTop: ScrollTop,
+            Band_H: Band_H,
+            S_V: S_V
+        }
+    };
+    function show(basicInfo) {
+        if (basicInfo.ScrollTop > 0.001) {
+            $(".neko").css('display', 'block');
+        } else {
+            $(".neko").css('display', 'none');
         }
     }
-
-    function b(a) {
-        a.ScrollTop > .001 ? $(".neko").css("display", "block") : $(".neko").css("display", "none"), a.Band_H > 40 ? ($("#catalog-box").css("display", "block"), a.Band_H > 70 ? ($("#catalog-box").css("display", "none"), $("#catalog-box2").css("display", "block")) : $("#catalog-box2").css("display", "none")) : ($("#catalog-box").css("display", "none"), $("#catalog-box2").css("display", "none"))
-    }! function ($) {
-        $.fn.nekoScroll = function (e) {
-            var f = {
-                    top: "0",
-                    scroWidth: "6px",
-                    z_index: 9999,
-                    zoom: .9,
-                    borderRadius: "5px",
-                    right: "60px",
-                    nekoImg: "https://bu.dusays.com/2022/07/20/62d812db74be9.png",
-                    hoverMsg: "\u55B5\u55B5\u55B5~",
-                    color: "#6f42c1",
-                    during: 500,
-                    blog_body: "body"
-                },
-                c = $.extend(f, e),
-                g = "" !== this.prop("className") ? "." + this.prop("className") : "" !== this.prop("id") ? "#" + this.prop("id") : this.prop("nodeName");
-            0 == $(".neko").length && (this.after('<div class="neko" id=' + c.nekoname + ' data-msg="' + c.hoverMsg + '"></div>'), $("#" + c.nekoname).append('<div id="catalog-box"><a class="link-open">\u6253\u8D4F\u732B\u54AA</div>   </div>'), $("#" + c.nekoname).append('<div id="catalog-box2">\u5982\u679C\u6211\u7684\u6587\u7AE0\u5E2E\u52A9\u5230\u4E86\u60A8\uFF0C\u53EF\u4EE5\u5E2E\u5FD9\u70B9\u4E00\u4E0B\u7AD9\u5185\u7684\u5C0F\u5E7F\u544A\u5417\uFF1F\u611F\u8C22\u60A8\u7684\u652F\u6301 \u{1F339}\u{1F339}\u{1F339}  </div>'));
-            let d = a();
-            return $(g).css({
-                position: "fixed",
-                width: c.scroWidth,
-                top: c.top,
-                height: d.Band_H * c.zoom * d.ViewH * .01 + "px",
-                "z-index": c.z_index,
-                "background-color": c.bgcolor,
-                "border-radius": c.borderRadius,
-                right: c.right,
-                "background-image": "url(" + c.scImg + ")",
-                "background-image": "-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)",
-                "border-radius": "2em",
-                "background-size": "contain"
-            }), $("#" + c.nekoname).css({
-                position: "fixed",
-                top: d.Band_H * c.zoom * d.ViewH * .01 - 50 + "px",
-                "z-index": 10 * c.z_index,
-                right: c.right,
-                "background-image": "url(" + c.nekoImg + ")"
-            }), b(a()), $(window).scroll(function () {
-                let d = a();
-                b(d), $(g).css({
-                    position: "fixed",
-                    width: c.scroWidth,
-                    top: c.top,
-                    height: d.Band_H * c.zoom * d.ViewH * .01 + "px",
-                    "z-index": c.z_index,
-                    "background-color": c.bgcolor,
-                    "border-radius": c.borderRadius,
-                    right: c.right,
-                    "background-image": "url(" + c.scImg + ")",
-                    "background-image": "-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)",
-                    "border-radius": "2em",
-                    "background-size": "contain"
-                }), $("#" + c.nekoname).css({
-                    position: "fixed",
-                    top: d.Band_H * c.zoom * d.ViewH * .01 - 50 + "px",
-                    "z-index": 10 * c.z_index,
-                    right: c.right,
-                    "background-image": "url(" + c.nekoImg + ")"
-                }), d.ScrollTop == d.S_V ? $("#" + c.nekoname).addClass("showMsg") : ($("#" + c.nekoname).removeClass("showMsg"), $("#" + c.nekoname).attr("data-msg", c.hoverMsg))
-            }), this.click(function (a) {
+    (function ($) {
+        $.fn.nekoScroll = function (option) {
+            var defaultSetting = {
+                top: '0',
+                scroWidth: 6 + 'px',
+                z_index: 9999,
+                zoom: 0.9,
+                borderRadius: 5 + 'px',
+                right: 60 + 'px',
+                nekoImg: "https://bu.dusays.com/2022/07/20/62d812db74be9.png",
+                hoverMsg: "喵喵喵~",
+                color: "#6f42c1",
+                during: 500,
+                blog_body: "body",
+            };
+            var setting = $.extend(defaultSetting, option);
+            var getThis = this.prop("className") !== "" ? "." + this.prop("className") : this.prop("id") !== "" ? "#" +
+                this.prop("id") : this.prop("nodeName");
+            if ($(".neko").length == 0) {
+                this.after("<div class=\"neko\" id=" + setting.nekoname + " data-msg=\"" + setting.hoverMsg + "\"></div>");
+            }
+            let basicInfo = getBasicInfo();
+            $(getThis)
+                .css({
+                    'position': 'fixed',
+                    'width': setting.scroWidth,
+                    'top': setting.top,
+                    'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
+                    'z-index': setting.z_index,
+                    'background-color': setting.bgcolor,
+                    "border-radius": setting.borderRadius,
+                    'right': setting.right,
+                    'background-image': 'url(' + setting.scImg + ')',
+                    'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
+                    'background-size': 'contain'
+                });
+            $("#" + setting.nekoname)
+                .css({
+                    'position': 'fixed',
+                    'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
+                    'z-index': setting.z_index * 10,
+                    'right': setting.right,
+                    'background-image': 'url(' + setting.nekoImg + ')',
+                });
+            show(getBasicInfo());
+            $(window)
+                .scroll(function () {
+                    let basicInfo = getBasicInfo();
+                    show(basicInfo);
+                    $(getThis)
+                        .css({
+                            'position': 'fixed',
+                            'width': setting.scroWidth,
+                            'top': setting.top,
+                            'height': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 + 'px',
+                            'z-index': setting.z_index,
+                            'background-color': setting.bgcolor,
+                            "border-radius": setting.borderRadius,
+                            'right': setting.right,
+                            'background-image': 'url(' + setting.scImg + ')',
+                            'background-image': '-webkit-linear-gradient(45deg, rgba(255, 255, 255, 0.1) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.1) 50%, rgba(255, 255, 255, 0.1) 75%, transparent 75%, transparent)', 'border-radius': '2em',
+                            'background-size': 'contain'
+                        });
+                    $("#" + setting.nekoname)
+                        .css({
+                            'position': 'fixed',
+                            'top': basicInfo.Band_H * setting.zoom * basicInfo.ViewH * 0.01 - 50 + 'px',
+                            'z-index': setting.z_index * 10,
+                            'right': setting.right,
+                            'background-image': 'url(' + setting.nekoImg + ')',
+                        });
+                    if (basicInfo.ScrollTop == basicInfo.S_V) {
+                        $("#" + setting.nekoname)
+                            .addClass("showMsg")
+                    } else {
+                        $("#" + setting.nekoname)
+                            .removeClass("showMsg");
+                        $("#" + setting.nekoname)
+                            .attr("data-msg", setting.hoverMsg);
+                    }
+                });
+            this.click(function (e) {
                 btf.scrollToDest(0, 500)
-            }), $("#" + c.nekoname).click(function () {
-                btf.scrollToDest(0, 500)
-            }), $(".link-open").click(function () {
-                window.open("https://nkupp.com/post/9366f1e2.html")
-            }), this
+            });
+            $("#" + setting.nekoname)
+                .click(function () {
+                    btf.scrollToDest(0, 500)
+                });
+            return this;
         }
-    }(jQuery), $(document).ready(function () {
+    })(jQuery);
+
+    $(document).ready(function () {
+        //部分自定义
         $("#myscoll").nekoScroll({
-            bgcolor: "rgb(255 255 255 / .3)",
-            borderRadius: "2em",
-            zoom: .9
-        })
+            bgcolor: 'rgb(0 0 0 / .5)', //背景颜色，没有绳子背景图片时有效
+            borderRadius: '2em',
+            zoom: 0.9
+        }
+        );
+        //自定义（去掉以下注释，并注释掉其他的查看效果）
+        /*
+        $("#myscoll").nekoScroll({
+            nekoname:'neko1', //nekoname，相当于id
+            nekoImg:'img/猫咪.png', //neko的背景图片
+            scImg:"img/绳1.png", //绳子的背景图片
+            bgcolor:'#1e90ff', //背景颜色，没有绳子背景图片时有效
+            zoom:0.9, //绳子长度的缩放值
+            hoverMsg:'你好~喵', //鼠标浮动到neko上方的对话框信息
+            right:'100px', //距离页面右边的距离
+            fontFamily:'楷体', //对话框字体
+            fontSize:'14px', //对话框字体的大小
+            color:'#1e90ff', //对话框字体颜色
+            scroWidth:'8px', //绳子的宽度
+            z_index:100, //不用解释了吧
+            during:1200, //从顶部到底部滑动的时长
+        });
+        */
     })
 }
